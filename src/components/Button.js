8ff802b119f20acccca2e863a2af914d98bcf77f
David Asensio Cañas <david.asensio@cabify.com>
2021-01-30 00:35:43 +0100

    Add Button component

---
+++ src/components/Button.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.button`
+  background: #FF6B6B;
+  border: none;
+  border-radius: 9999px;
+  cursor: pointer;
+  outline: none;
+  padding: 1em 2em;
+  width: 100%;
+
+  transition: background 300ms;
+
+  &:hover {
+    background: #CA3A3A;
+  }
+`
+
+const Text = styled.span`
+  color: #FFF;
+  font-family: 'Roboto', sans-serif;
+  font-size: 24px;
+  font-weight: 900;
+  text-transform: uppercase;
+`
+
+export function Button ({ children, onClick }) {
+  return (
+    <Wrapper onClick={onClick}>
+      <Text>{children}</Text>
+    </Wrapper>
+  )
+}
